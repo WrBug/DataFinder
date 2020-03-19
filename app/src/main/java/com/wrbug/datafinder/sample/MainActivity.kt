cbe5b25436f26215067b4615c5b8b290eada1e07
WrBug <i@wrbug.com>
2020-03-19 16:59:05 +0800

    更新web

--- app/src/main/java/com/wrbug/datafinder/sample/MainActivity.kt
+++ app/src/main/java/com/wrbug/datafinder/sample/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.wrbug.datafinder.sample
 
+import android.Manifest
+import android.os.Build
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
@@ -8,5 +10,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            requestPermissions(arrayOf(Manifest.permission.CALL_PHONE,Manifest.permission.READ_PHONE_STATE),1)
+        }
     }
 }
